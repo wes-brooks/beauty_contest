0e2d821b95ddb43f96307ae21a3bdbdcadcc23bf
Wesley Brooks <brooks.wesley@gmail.com>
2012-08-23 19:23:50 -0500

    glimp is mostly workig. results are not precisely equal to glmnet, tho

--- R/glimp/glimp.r
+++ R/glimp/glimp.r
@@ -1,10 +1,9 @@
 glimp = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100, nlambda=100, lambda.min.ratio=100, lambda=NULL) {
-#Drop any rows with NA values
+    #Drop any rows with NA values
     data = data
     na.rows = (which(is.na(data))-1) %% dim(data)[1] + 1
     if (length(na.rows)>0)
         data = data[-na.rows,]
-    result[['data']] = data
 
     #Pull out the relevant data
     response.name = rownames(attr(terms(formula, data=data), 'factors'))[1]
@@ -17,7 +16,7 @@ glimp = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100, n
     
     meanx = vector()
     normx = vector()
-    ssx = vector()
+
     for (k in 1:ncol(x)) {
         meanx = c(meanx, mean(x[,k]))
         normx = c(normx, sqrt(sum((x[,k]-meanx[k])**2)))
@@ -25,84 +24,101 @@ glimp = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100, n
     }
     
     x = cbind(rep(1,nrow(x)), x)
-        
-    result[['response']] = response.name
-    result[['predictors']] = predictor.names
+
+    s = svd(x)   #  if you don't know what svd is, you should!  It's great.
+    F = s$u  %*% diag(1/s$d)  %*%  t(s$u)
+    x = F%*%x
+    #pY = F%*%z
+    #fit = lars(pX, pY)   #  You could replace "lars" with your favorite sparse regression function.  I have tried 
     
-    eps = 0.005
-    yy = ifelse(y<eps, eps, ifelse(y>1-eps, 1-eps, y))
-    eta = log(yy) - log(1-yy)
-    
-    #Define some constants
     if (is.null(lambda)) {
-        lmax = max(abs(cor(x[,-1],eta))) * sqrt(mean((eta-mean(eta))**2))
-        ll = seq(from=lmax, to=lmax/lambda.min.ratio, length.out=nlambda)
+        lmax = max(abs(cor(x[,-1],y))) * sqrt(mean((y-mean(y))**2))
+        ll = seq(from=5*lmax, to=lmax/lambda.min.ratio, length.out=nlambda)
     } else {        
         ll = as.vector(lambda)
     }
-    
+
+    eps = 1e-5
+    eps.inv = log(eps) - log(1-eps)
+    b = matrix(0, ncol(x), 1)
+    b[1] = log(mean(y)) - log(1-mean(y))
+
     beta = list()
-    b = matrix(rnorm(n=ncol(x)), ncol(x), 1)
-    b[1] = mean(eta)
-    
-    if (family=='binomial') {
-        for (j in 1:length(ll)) {
-            l = ll[j]
-            if (j>1) {b=beta[[j-1]]}  
 
-            eta = x %*% b
-            fitted = exp(eta) / (1+exp(eta))
-            fitted = ifelse(fitted<eps, eps, ifelse(fitted>1-eps,1-eps,fitted))            
-            W = as.vector(fitted * (1-fitted))
-            z = eta + (y-fitted) / (fitted*(1-fitted))
-            
-            obj.old = 0
-            iter=0
-            finished = FALSE
-            
-            while (finished == FALSE) {
-            
-                for (k in 2:length(b)) {
-                    partial = z - eta + x[,k]*b[k]                    
-                    b[k] = S(sum(W*(partial-mean(partial))*x[,k]), l*sqrt(n)) / sum(W*x[,k]**2)
-                    
-                    eta = x %*% b
-                    fitted = exp(eta) / (1+exp(eta))
-                    z = eta + (y-fitted) / (fitted*(1-fitted))
-                    W = as.vector(fitted * (1-fitted))
-                }
+    for (j in 1:length(ll)) {
+        l = ll[j]
+        if (j>1) {b=beta[[j-1]]}  
+
+        eta = x %*% b
+        fitted = ifelse(eta<eps.inv, eps, ifelse(eta>-eps.inv, 1-eps, exp(eta)/(1+exp(eta))))
+        w = as.vector(fitted*(1-fitted))
+
+        obj.old = 0
+        iter=0
+        finished = FALSE
+
+        while (finished==FALSE) {
+            #print(fitted)
+            #print(w)
+    
+            #z = as.matrix((y-fitted)/w + eta, nrow(x), 1)
+            #b = solve(t(x) %*% diag(w) %*% x) %*% t(x) %*% diag(w) %*% z
+            #print(b)
+            for (k in 2:length(b)) {
+                #print(w)
+                z = (y-fitted)/w + x[,k]*b[k]
                 
-                partial = z - eta + x[,k]*b[k]
-                b[1] = mean(partial)
+                z = F%*%z
+
+                b[k] = lsfit(x=as.matrix(x[,k], nrow(x), 1), y=as.matrix(z, nrow(x), 1), wt=as.vector(w), intercept=TRUE)$coef[2]
+                #b[k] = sum(w*(z-mean(z))*x[,k]) / sum(w*x[,k]**2)
+                b[k] = S(b[k], l*sqrt(n)) 
+            
                 eta = x %*% b
-                fitted = exp(eta) / (1+exp(eta))
-                z = eta + (y-fitted) / (fitted*(1-fitted))
-                W = as.vector(fitted * (1-fitted))                
-            
-                obj = mean(W * (z - x%*%b)**2) + l*sum(abs(b[-1]))
-                
-                print(obj)
-                if(is.na(obj)) {return(list(z=z, eta=eta, x=x, b=b, y=y, partial=partial, lambda=l, W=W))}
-                
-                iter = iter + 1
-                if (abs(obj-obj.old)<tol || iter>=max.iter) {finished = TRUE}
-                obj.old = obj
+                #print(eta)
+                fitted = ifelse(eta<eps.inv, eps, ifelse(eta>-eps.inv, 1-eps, exp(eta) / (1+exp(eta))))
+                #fitted = as.vector(exp(eta) / (1+exp(eta)))
+                #fitted = ifelse(fitted<eps, eps, ifelse(fitted>1-eps, 1-eps, fitted))
+                w = fitted*(1-fitted)
             }
-            
-            cat(paste("Iterations: ", iter, "\n", sep=""))
-            beta[[j]] = b
-            
-            for (i in 1:length(beta)) {
-                beta[[i]][-1] = beta[[i]][-1] / normx
-                beta[[i]][1] = beta[[i]][1] - sum(beta[[i]][-1]*meanx)
-
-                rownames(beta[[i]]) = c("(Intercept)", predictor.names)
-                beta[[i]] = Matrix(beta[[i]])
-            }
-        }
-    }
+            #print(fitted)
+            #print(w)
+            z = (y-fitted)/w + x[,1]*b[1]
+            z = F%*%z
+            b[1] = mean(z)
     
-    return(list(beta=beta, meanx=meanx, normx=normx, x=x, lambda=ll))
+            #print(z)
+            #print(b)
+    
+            eta = x %*% b
+            fitted = ifelse(eta<eps.inv, eps, ifelse(eta>-eps.inv, 1-eps, exp(eta)/(1+exp(eta))))
+            #fitted = as.vector(exp(eta) / (1+exp(eta)))
+            #fitted = ifelse(fitted<eps, eps, ifelse(fitted>1-eps, 1-eps, fitted))
+            w = as.vector(fitted*(1-fitted))
+    
+            #print(eta)            
+    
+            obj = sum(w*(y*eta - (1+exp(eta)))) #+ l*sum(abs(b[-1]))
+            
+            iter = iter+1
+            cat(paste("obj=", obj, ", obj.old=", obj.old, "\n", sep=""))
+            if (abs(obj-obj.old)<tol || iter>=max.iter) {finished = TRUE}
+            obj.old = obj
+        }
+    
+        cat(paste("Iterations: ", iter, "\n", sep=""))
+        beta[[j]] = b
+    }
+
+    for (i in 1:length(beta)) {
+        beta[[i]][-1] = beta[[i]][-1] / normx
+        beta[[i]][1] = beta[[i]][1] - sum(beta[[i]][-1]*meanx)
+
+        rownames(beta[[i]]) = c("(Intercept)", predictor.names)
+        beta[[i]] = Matrix(beta[[i]])
+    }
+
+	return(list(beta=beta, meanx=meanx, normx=normx, x=x, lambda=ll))
 }
  
     
@@ -117,17 +133,11 @@ S = function(z, gamma) {
 
 
 logistic = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100) {
-    #Create the object that will hold the output
-    result = list()
-    class(result) = "glimp"
-    result[['formula']] = as.formula(formula, env=data)
-    
     #Drop any rows with NA values
     data = data
     na.rows = (which(is.na(data))-1) %% dim(data)[1] + 1
     if (length(na.rows)>0)
         data = data[-na.rows,]
-    result[['data']] = data
 
     #Pull out the relevant data
     response.name = rownames(attr(terms(formula, data=data), 'factors'))[1]
@@ -136,48 +146,89 @@ logistic = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100
     
     y = as.matrix(data[,response.col])
     x = as.matrix(data[,predictor.names])
-    #x = scale(x, center=TRUE, scale=FALSE)
-    #meanx = attr(x, "scaled:center")
-    #normx = 1 / attr(x, "scaled:scale")
+    n=nrow(x)
+    
+    meanx = vector()
+    normx = vector()
+
+    for (k in 1:ncol(x)) {
+        meanx = c(meanx, mean(x[,k]))
+        normx = c(normx, sqrt(sum((x[,k]-meanx[k])**2)))
+        x[,k] = (x[,k]-meanx[k])/normx[k]
+    }
     
     x = cbind(rep(1,nrow(x)), x)
     
-    result[['response']] = response.name
-    result[['predictors']] = predictor.names
-    
-    #Define some constants
-    eps = 1e-5    
-    
-    if (family=='binomial') {
-        for (j in 1:length(ll)) {
-            lambda = ll[j]
-            
-            obj.old = 0
-            iter=0
-            finished = FALSE
-            
-            while (finished==FALSE) {
-                b = lsfit(x=x, y=z, wt=W, intercept=FALSE)$coef
-                print(b)
-                eta = x %*% b
-                fitted = exp(eta) / (1+exp(eta))
-                z = eta + (y-fitted) / (fitted*(1-fitted))
-                W = as.vector(fitted * (1-fitted))
-                
-                lq = -0.5 * mean(W * (z - x%*%b)**2)
-                obj = -lq #+ penalty(alpha, b[-1], lambda)  
-                print(lq)
-                
-                iter = iter + 1
-                if (abs(obj - obj.old)<tol || iter>=max.iter) {finished = TRUE}
-                obj.old = obj
-            }
-            
-            cat(paste("Iterations: ", iter, "\n", sep=""))
-            return(b)
+    eps = 1e-5
+    eps.inv = log(eps) - log(1-eps)
+    b = matrix(0, ncol(x), 1)
+    b[1] = log(mean(y)) - log(1-mean(y))
+
+    eta = x %*% b
+    fitted = ifelse(eta<eps.inv, eps, ifelse(eta>-eps.inv, 1-eps, exp(eta)/(1+exp(eta))))
+    w = as.vector(fitted*(1-fitted))
+
+    obj.old = 0
+    iter=0
+    finished = FALSE
+
+    while (finished==FALSE) {
+        #print(fitted)
+        #print(w)
+
+        #z = as.matrix((y-fitted)/w + eta, nrow(x), 1)
+        #b = solve(t(x) %*% diag(w) %*% x) %*% t(x) %*% diag(w) %*% z
+        #print(b)
+        for (k in 2:length(b)) {
+            #print(w)
+            z = (y-fitted)/w + x[,k]*b[k]
+            b[k] = lsfit(x=as.matrix(x[,k], nrow(x), 1), y=as.matrix(z, nrow(x), 1), wt=as.vector(w), intercept=TRUE)$coef[2]
+            #b[k] = sum(w*(z-mean(z))*x[,k]) / sum(w*x[,k]**2)
+            #b[k] = S(sum(w*(z-mean(z))*x[,k]) / sum(, l*sqrt(n)) 
+        
+            eta = x %*% b
+            #print(eta)
+            fitted = ifelse(eta<eps.inv, eps, ifelse(eta>-eps.inv, 1-eps, exp(eta) / (1+exp(eta))))
+            #fitted = as.vector(exp(eta) / (1+exp(eta)))
+            #fitted = ifelse(fitted<eps, eps, ifelse(fitted>1-eps, 1-eps, fitted))
+            w = fitted*(1-fitted)                     
         }
+        #print(fitted)
+        #print(w)
+        z = (y-fitted)/w + x[,1]*b[1]
+        b[1] = mean(z)
+
+        #print(z)
+        #print(b)
+
+        eta = x %*% b
+        fitted = ifelse(eta<eps.inv, eps, ifelse(eta>-eps.inv, 1-eps, exp(eta)/(1+exp(eta))))
+        #fitted = as.vector(exp(eta) / (1+exp(eta)))
+        #fitted = ifelse(fitted<eps, eps, ifelse(fitted>1-eps, 1-eps, fitted))
+        w = as.vector(fitted*(1-fitted))
+
+        #print(eta)            
+
+        obj = sum(w*(y*eta - (1+exp(eta)))) #+ l*sum(abs(b[-1]))
+        
+        iter = iter+1
+        cat(paste("obj=", obj, ", obj.old=", obj.old, "\n", sep=""))
+        if (abs(obj-obj.old)<tol || iter>=max.iter) {finished = TRUE}
+        obj.old = obj
     }
+    
+    cat(paste("Iterations: ", iter, "\n", sep=""))
+    beta = b
+    
+    beta[-1] = beta[-1] / normx
+    beta[1] = beta[1] - sum(beta[-1]*meanx)
+
+    #rownames(beta) = c("(Intercept)", predictor.names)
+    beta = Matrix(beta)
+
+	return(list(beta=beta, meanx=meanx, normx=normx, x=x, fitted=fitted))
 }
+
 
 
 lasso = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100, nlambda=100, lambda.min.ratio=100, lambda=NULL) {
@@ -186,7 +237,6 @@ lasso = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100, n
     na.rows = (which(is.na(data))-1) %% dim(data)[1] + 1
     if (length(na.rows)>0)
         data = data[-na.rows,]
-    result[['data']] = data
 
     #Pull out the relevant data
     response.name = rownames(attr(terms(formula, data=data), 'factors'))[1]
@@ -207,9 +257,6 @@ lasso = function(formula, data, family, weights=NULL, tol=1e-10, max.iter=100, n
     }
     
     x = cbind(rep(1,nrow(x)), x)
-        
-    result[['response']] = response.name
-    result[['predictors']] = predictor.names
     
     if (is.null(lambda)) {
         lmax = max(abs(cor(x[,-1],y))) * sqrt(mean((y-mean(y))**2))
