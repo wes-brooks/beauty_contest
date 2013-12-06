36dfd4db3761212f3161cf203add148ff552004f
Wesley Brooks <brooks.wesley@gmail.com>
2013-12-06 16:10:23 -0600

    build libraries then copy them to chtc

--- chtc/beautydata/shared/R-libs/galm/R/predict.galm.R
+++
@@ -1,8 +0,0 @@
-predict.galm <-
-function(obj, newx, ...) {
-    predictors = obj[['vars']]
-    data = newx[,predictors]
-    
-    out = predict(obj=obj[['model']], newdata=data)
-    return(out)
-}
