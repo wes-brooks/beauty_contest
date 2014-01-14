44cb7c40d2c9974e144f2c6b22a1acdfd53a4c4e
Wesley Brooks <brooks.wesley@gmail.com>
2014-01-14 12:59:59 -0600

    fixed a typo (missing comma) in loo-atomic.r

--- R/beauty-atomic.r
+++ R/beauty-atomic.r
@@ -23,37 +23,18 @@ install.packages("R-libs/galogistic", repos=NULL, type='source')
 install.packages("R-libs/spls.wrap", repos=NULL, type='source')
 
 sink("result.txt", append=TRUE)
-cat(paste('installations complete', "\n", sep=''))
-sink()
-
-sink("result.txt", append=TRUE)
 #These are the modeling modules:
 source('gbm.r')
-cat("loaded gbm\n")
 source('pls.r')
-cat("loaded pls\n")
 source('galm.r')
-cat("loaded galm\n")
 source('galogistic.r')
-cat("loaded galogistic\n")
 source('adapt.r')
-cat("loaded adapt\n")
 source('adalasso.r')
-cat("loaded adalasso\n")
 source('spls.r')
-cat("loaded spls\n")
-sink()
-
-sink("result.txt", append=TRUE)
-cat(paste('going to settings', "\n", sep=''))
 sink()
 
 #Import location and modeling settings:
 source('settings.r')
-
-sink("result.txt", append=TRUE)
-cat(paste('going to utils', "\n", sep=''))
-sink()
 
 #Import some necessary functions:
 source('utils.r')
@@ -65,16 +46,8 @@ sink()
 #Load the process ID from the jobid.txt file
 seeds = read.table("seeds.txt")
 
-sink("result.txt", append=TRUE)
-cat(paste('got seeds', "\n", sep=''))
-sink()
-
 args = scan('params.txt', 'character')
 args = strsplit(args, '\\n', fixed=TRUE)[[1]]
-
-sink("result.txt", append=TRUE)
-cat(paste('jobid:', args[4], "\n", sep=''))
-sink()
 
 type = args[1]
 cluster = args[2]
@@ -85,8 +58,10 @@ process = as.numeric(args[5])
 seed = (1000 * seeds[process,]) %/% 1
     
 sink("result.txt", append=TRUE)
+cat(paste('type: ', type, "\n", sep=''))
 cat(paste('location: ', beach, "\n", sep=''))
 cat(paste('method: ', method, "\n", sep=''))
+cat(paste('process: ', process, "\n", sep=''))
 sink()
     
 result = "placeholder"
@@ -96,7 +71,7 @@ output = ""
 prefix = paste(cluster, process, sep=".")
 
 sink("result.txt", append=TRUE)
-cat(paste(prefix, "\n", sep=''))
+cat(paste("output prefix: ", prefix, "\n", sep=''))
 sink()
 
 #Use the process number to determine whether we'll run loo or annual, and with which fold
