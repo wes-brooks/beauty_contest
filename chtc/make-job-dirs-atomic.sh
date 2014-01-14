7e98455f8bf06bb172e90da89d9d53bc89d97df3
Wesley Brooks <wbrooks2@submit-1.chtc.wisc.edu>
2014-01-14 13:00:43 -0600

    fixed the arithmetic in make-job-dirs-atomic.sh

--- chtc/make-job-dirs-atomic.sh
+++ chtc/make-job-dirs-atomic.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 for ((i=1; i<=$1; i++))
 do
@@ -6,9 +6,11 @@ do
   echo "loo\n$2\n$3\n$4\n$i" > beautydata-$3-$4/$i/params.txt
 done
 
+
 for ((i=1; i<=4; i++))
 do
-  k = $(( $i + $1 ))
+  ((k = $i + $1))
+  echo $k
   mkdir -p beautydata-$3-$4/$k
   echo "annual\n$2\n$3\n$4\n$i" > beautydata-$3-$4/$k/params.txt
 done
