c2d1f58719d8fac5886df6b99f51cd044abda71c
Wesley Brooks <brooks.wesley@gmail.com>
2014-02-03 17:30:12 -0600

    $site and $method need to be variables, duh!

--- chtc/boot.sh
+++ chtc/boot.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
-sites=( hika maslowski kreher thompson point neshotah redarrow )
-methods=( pls gbm gbmcv galogistic-unweighted galogistic-weighted adalasso-unweighted adalasso-unweighted-select adalasso-weighted adalasso-weighted-select galm adapt adapt-select spls spls-select )
+#sites=( hika maslowski kreher thompson point neshotah redarrow )
+#methods=( pls gbm gbmcv galogistic-unweighted galogistic-weighted adalasso-unweighted adalasso-unweighted-select adalasso-weighted adalasso-weighted-select galm adapt adapt-select spls spls-select )
+sites=( hika maslowski )
+methods=( pls adapt )
 
 for site in ${sites[@]}
 do
   for method in ${methods[@]}
   do
-    make atomic SITE=site METHOD=method
+    make atomic SITE=$site METHOD=$method
   done
 done
