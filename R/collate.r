d3442025bfdd317c64d07b647d9986056b386a87
Wesley Brooks <brooks.wesley@gmail.com>
2014-02-07 20:55:29 -0600

    changes to the collate script

--- R/collate.r
+++ R/collate.r
@@ -1,27 +1,43 @@
-predicted = vector()
-actual = vector()
-threshold = vector()
-fold = vector()
+require(stringr)
 
-for (f in files) {
-  raw = scan(file, 'character', sep='\n')
-  i = grep("^# Site = ([^\\w])", raw)
-  if (i) {
-    site = str_match(raw, "^# Site = ([^\\w]+$)")[i,2]
+sites = c('hika', 'maslowski', 'kreher', 'thompson', 'point', 'neshotah', 'redarrow')
+methods = c('pls', 'gbm', 'gbmcv', 'galogistic-unweighted', 'galogistic-weighted', 'adalasso-unweighted', 'adalasso-unweighted-select', 'adalasso-weighted', 'adalasso-weighted-select', 'galm', 'adapt', 'adapt-select', 'spls', 'spls-select')
+
+sites = c("thompson")
+methods = c("gbmcv")
+
+results = list()
+for (site in sites) {
+  site_results = list()
+  for (method in methods) {
+    indx = grep(paste("^beautyrun\\.\\d+\\.", "thompson", "\\.", "gbmcv", "\\.out", sep=""), dir(), perl=TRUE)
+    files = dir()[indx]
+    
+    predicted = vector()
+    actual = vector()
+    threshold = vector()
+    fold = vector()
+    
+    for (f in files) {
+      raw = scan(file, 'character', sep='\n')
+      
+      i = grep("^# predicted:", raw)
+      predicted = c(predicted, as.numeric(raw[i+1]))
+      
+      i = grep("^# actual:", raw)
+      actual = c(actual, as.numeric(raw[i+1]))
+      
+      i = grep("^# fold:", raw)
+      fold = c(fold, as.numeric(raw[i+1]))
+      
+      i = grep("^# threshold:", raw)
+      threshold = c(threshold, as.numeric(raw[i+1]))
+      
+      i = grep("^# vars:", raw)
+      vars = c(vars, strsplit(raw[i+1], split=", "))
+    }
+    
+    site_results[[method]] = list(predicted, actual, fold, threshold, vars)
   }
-  
-  i = grep("^# Method = ([[:alnum:][:punct:]])", raw)
-  if (i) {
-      method = str_match(raw, "^# Method = ([[:alnum:][:punct:]]+$)")[i,2]
-  }
-  
-  i = grep("^\\$predicted$", raw)
-  if (i) {
-      pred = as.numeric(raw[i+2])
-  }
-  
-  i = grep("^\\$actual$", raw)
-  if (i) {
-      act = as.numeric(strsplit(raw[i+1], " ")[[1]][2])
-  }
+  results[[site]] = site_results
 }
