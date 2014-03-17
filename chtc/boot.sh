675837934dd7d9a59aec0349ad2cb55c3b139c32
Wesley Brooks <brooks.wesley@gmail.com>
2014-03-17 15:25:28 -0500

    starting pls

--- chtc/boot.sh
+++ chtc/boot.sh
@@ -2,7 +2,7 @@
 
 sites=( hika maslowski kreher thompson point neshotah redarrow )
 #methods=( pls gbm gbmcv galogistic-unweighted galogistic-weighted adalasso-unweighted adalasso-unweighted-select adalasso-weighted adalasso-weighted-select galm adapt adapt-select spls spls-select )
-methods=( adapt adapt-select )
+methods=( pls )
 
 
 for site in ${sites[@]}
