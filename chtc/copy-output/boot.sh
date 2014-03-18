cb96ee4876c9a0ddf22178a4cc4c1850a77ef95d
Wesley Brooks <brooks.wesley@gmail.com>
2014-03-18 10:59:28 -0500

    logging the results of copying beauty contest results

--- chtc/copy-output/boot.sh
+++ chtc/copy-output/boot.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+mkdir -p copy-records
+
 sites=( hika maslowski kreher thompson point neshotah redarrow )
 methods=( pls gbm gbmcv galogistic-unweighted galogistic-weighted adalasso-unweighted adalasso-unweighted-select adalasso-weighted adalasso-weighted-select galm adapt adapt-select spls spls-select )
 
@@ -7,6 +9,6 @@ for site in ${sites[@]}
 do
   for method in ${methods[@]}
   do
-    ./get.sh $site $method
+    ./get.sh $site $method > copy-records/$site-$method.txt
   done
 done
