3ea3258a6fe6fcfc65fa7e9f89f8437672567539
Wesley Brook <brook@desk01.stat.wisc.edu>
2014-03-19 11:13:39 -0500

    scratch script to run mutiple analyses

--- scratch/beauty-atomic-scratch.r
+++ scratch/beauty-atomic-scratch.r
@@ -6,7 +6,7 @@ type='loo'
 cluster = NA
 beach = 'point'
 method = 'spls'
-process = 189
+processes = c(189
 
 result = "placeholder"
 output = ""
@@ -14,6 +14,8 @@ seed = 1
 
 prefix = paste("~/scratch/beautyrun", process, sep=".")
 
-source("R/loo-atomic.r")
 
+for (process in processes) {
+  source("R/loo-atomic.r")
+}
 
