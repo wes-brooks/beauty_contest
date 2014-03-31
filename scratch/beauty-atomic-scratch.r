f292059a5f8b329ccd80997b29f2e6a4cac184b9
Wesley Brooks <brooks.wesley@gmail.com>
2014-03-31 12:42:41 -0500

    manually generating the last of the leave one year out results

--- scratch/beauty-atomic-scratch.r
+++ scratch/beauty-atomic-scratch.r
@@ -2,11 +2,11 @@ source("scratch/import-modules.r")
 source("R/settings.r")
 source('R/utils.r')
 
-type='loo'
+type='annual'
 cluster = NA
 beach = 'point'
 method = 'spls'
-processes = c(139, 149, 156, 174, 175, 177, 183, 185, 188)
+processes = c(1)
 
 result = "placeholder"
 output = ""
@@ -15,7 +15,19 @@ seed = 1
 for (process in processes) {
   prefix = paste("~/scratch/beautyrun", process, sep=".")
   cat(paste("process: ", process, "\n", sep=""))
-  source("R/loo-atomic.r")
+  
+  #Use the process number to determine whether we'll run loo or annual, and with which fold
+  if (type=='loo') {
+      source("R/loo-atomic.r")
+  }
+  
+  if (type=='annual') {
+      source('R/annual-atomic.r')
+  }
+  
+  if (type=='final') {
+      source('R/final-atomic.r')
+  }
 }
 
 
