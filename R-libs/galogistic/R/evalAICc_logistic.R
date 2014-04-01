50cd6f182c28a7c9002d98846f3e31980f3187b9
Wesley Brooks <brooks.wesley@gmail.com>
2014-04-01 12:34:02 -0500

    evalAICc_logistic.r will now look specifically for warnings that indicate the algorithm didn't converge, or converged to exactly zero or one for some observations.

--- R-libs/galogistic/R/evalAICc_logistic.R
+++ R-libs/galogistic/R/evalAICc_logistic.R
@@ -1,25 +1,23 @@
-evalAICc_logistic <-
-    function(chromosome=c(), data, output, family, weights) {
-        returnVal = Inf
-        minLV = 1
-        if (sum(chromosome) >= minLV) {
-            #Extract the selected variables, create the model formula and fit the model
-            out.col = which(colnames(data)==output)
-            selected = data.frame(output=data[,output], data[,-out.col][,chromosome==1])
-            f = as.formula("output~.")
-            returnVal = tryCatch(
-{
-    model = glm(f, data=selected, family, weights)
+evalAICc_logistic <- function(chromosome=c(), data, output, family, weights) {
+    returnVal = Inf
+    minLV = 1
+    if (sum(chromosome) >= minLV) {
+        #Extract the selected variables, create the model formula and fit the model
+        out.col = which(colnames(data)==output)
+        selected = data.frame(output=data[,output], data[,-out.col][,chromosome==1])
+        f = as.formula("output~.")
+
+        model = glm(f, data=selected, family, weights)
+        if ("glm.fit: algorithm did not converge" %in% names(warnings()) || "glm.fit: fitted probabilities numerically 0 or 1 occurred" %in% names(warnings())) {
+            returnVal = Inf
+        } else {
+            #Evaluate the model on the basis of the BIC:
+            n = nrow(selected)
+            summ = summary(model)
+            df = summ[['df.null']] - summ[['df.residual']]
     
-    #Evaluate the model on the basis of the BIC:
-    n = nrow(selected)
-    summ = summary(model)
-    df = summ[['df.null']] - summ[['df.residual']]
-    
-    AIC(model) + 2*df*(df+1)/(n-df-1)
-}, warning = function(w) {
-    Inf
-})
+            returnVal = AIC(model) + 2*df*(df+1)/(n-df-1)
         }
-return(returnVal)
     }
+    return(returnVal)
+}
