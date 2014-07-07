34afc8dd86863eec02988b44591985c5469b524d
Wesley Brooks <brooks.wesley@gmail.com>
2014-07-07 07:53:10 -0500

    all data imported

--- scratch/check-for-missing-results.r
+++ scratch/check-for-missing-results.r
@@ -1,3 +1,4 @@
+#annual:
 for (site in sites) {
     n = NULL
     
@@ -17,3 +18,29 @@ for (site in sites) {
     }
 }
         
+
+#LOO
+for (site in sites) {
+    n = NULL
+    
+    for (method in methods) {
+        rr = results[[site]][[method]][['res']]
+        
+        #Check whether all four folds are represented:
+        if (site == 'point') {
+            if (length(unique(rr$fold)) != 191 | nrow(rr) != 562) {
+                cat(paste(site, method, paste(sort(unique(rr$fold)), collapse=","), "\n", sep=" "))
+            }
+        } else {
+            if (length(unique(rr$fold)) != nrow(rr)) {
+                cat(paste(site, method, paste(sort(unique(rr$fold)), collapse=","), "\n", sep=" "))
+            }
+        }
+        
+        #if (is.null(n)) {
+        #    n = nrow(rr)
+        #} else if (n != nrow(rr)) {
+        #    cat(paste(site, method, n, "\n", sep=" "))
+        #}
+    }
+}
