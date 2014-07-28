45a391e93f997830129da24c51bf5b1764b1ec62
Wesley Brooks <brooks.wesley@gmail.com>
2014-07-28 13:53:27 -0500

    updating README

--- writeup/compile.sh
+++ writeup/compile.sh
@@ -3,9 +3,9 @@ mkdir -p aux
 
 lyx --export pdflatex Beauty_Contest.lyx
 
-xelatex -output-directory=aux Beauty_Contest
+xelatex -output-directory=aux-latex Beauty_Contest
 bibtex Beauty_Contest
-xelatex -output-directory=aux Beauty_Contest
-xelatex -output-directory=aux Beauty_Contest
+xelatex -output-directory=aux-latex Beauty_Contest
+xelatex -output-directory=aux-latex Beauty_Contest
 
-mv aux/Beauty_Contest.pdf ./
+mv aux-latex/Beauty_Contest.pdf ./
